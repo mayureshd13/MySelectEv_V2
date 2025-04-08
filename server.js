28b35161a1116a61f205a9033f2f22d60e07dd1d
Mayuresh Durgule <mayureshd1303@gmail.com>
2025-04-08 17:22:19 +0530

    cors update

--- server.js
+++ server.js
@@ -11,12 +11,21 @@ const EV = require("./models/EV");
 const { timeLog } = require("console");
 const { title } = require("process");
 require('dotenv').config();
+const cors = require("cors");
+
 
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.error("MongoDB Connection Error:", err));
+
+
+    app.use(cors({
+      origin: "https://select-ev.onrender.com"
+    }));
+
+    app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
